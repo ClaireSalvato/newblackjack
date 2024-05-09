5f76881aae92dc702d49eb7325317f9696fc6e17
Claire <148796047+ClaireSalvato@users.noreply.github.com>
2024-05-09 09:03:22 -0400

    we shuffled our deck

--- src/Card.java
+++ src/Card.java
@@ -10,42 +10,46 @@ public class Card {
 
         if(pType == 0) {
             type = "Ace";
+            value = 11;
         } else if (pType == 1){
-            type = "One";
+            type = "two";
+            value = 1;
         } else if(pType == 2){
-            type = "Two";
-        } else if (pType == 3){
             type = "Three";
-        } else if (pType == 4){
+            value = 2;
+        } else if (pType == 3){
             type = "Four";
-        } else if (pType == 5){
+            value = 3;
+        } else if (pType == 4){
             type = "Five";
-        } else if (pType == 6){
+            value = 4;
+        } else if (pType == 5){
             type = "Six";
-        } else if (pType == 7) {
+            value = 5;
+        } else if (pType == 6){
             type = "Seven";
+            value = 6;
+        } else if (pType == 7) {
+            type = "Eight";
+            value = 7;
         } else if (pType == 8){
-        type = "Eight";
-        } else if (pType == 9){
             type = "Nine";
-        } else if (pType == 10){
+            value = 8;
+        } else if (pType == 9) {
             type = "Ten";
+            value = 9;
+        } else if (pType == 10){
+            type = "King";
+            value = 10;
 
         }
 
         if(pSuit == 1){
-            suit = "Spades";
-        } else if (pSuit == 2){
-            suit = "Hearts";
-        } else if (pSuit == 3){
-            suit = "Clubs";
-        } else if (pSuit == 4){
-            suit = "Diamonds";
+
+
         }
 
-//        suit = pSuit;
-//        type = pType;
-        //value =
+
 
 
     }
--- src/Casino.java
+++ src/Casino.java
@@ -8,19 +8,50 @@ public class Casino {
     }
     public Casino(){
         System.out.println("Hello and welcome to my card game :)");
+        makeDeck();
+        shuffle();
+
+            }
+
+
+
+    public void makeDeck() {
         deck = new Card[52];
         int count = 0;
 
-    //    deck[0]= new Card("Ace","Spades");
-        for(int t = 0; t<13;t++){
-            for(int s = 0; s<4; s++){
-                if(t==0){
-                    deck[count] = new Card(t,s);
-                }
+        //    deck[0]= new Card("Ace","Spades");
+        for (int t = 0; t < 13; t++) {
+            for (int s = 0; s < 4; s++) {
+                //  if(t==0){
+                deck[count] = new Card(t, s);
+                deck[count].print();
+                count++;
 
             }
         }
 
+
+    }
+
+    public void shuffle(){
+        for(int x = 0; x < 52;x++){
+            int randomIndex = (int)(Math.random()*52);
+            Card randomCard = deck[randomIndex];
+
+            Card temp = deck[x];
+            deck[x] = randomCard;
+            deck[randomIndex] = temp;
+
+            printDeck();
+
+        }
+
+    }
+
+    public void printDeck(){
+        for(int i = 0; i < deck.length; i++){
+            deck[i].print();
+        }
     }
 
 
